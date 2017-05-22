027b409749d7bbb1c87a29cfff90d82d5221c319
PythonidaeJuliusDomitor <misha.smit@xs4all.nl>
2017-05-22 18:36:45 +0200

    Update 1.3
    -Added comments in H6

--- H6.py
+++ H6.py
@@ -1,14 +1,19 @@
 ##Opdracht:
-##Maak een programma dat een willekeurig vermenigvuldigings-som maakt, dus elk_getal * elk_ander_getal en dat vijf keer doet en de som + het antwoord print.
-##Maar na elke print moet hij het scherm legen, na 2 seconden.
-import os, random, time
+##Schrijf een programma dat met 2 willekeurige getallen een vermenigvuldiging maakt en de som + het antwoord print, dit moet hij 5 keer uitvoeren.
+##Zorg er ook voor dat hij na elke print, 2 seconden wacht en dan het scherm leegt.
+##Extra: pas het programma zo aan dat hij dit een willekeurig aantal keer doet.
 
-i = 0
-while i < 5:
-	getal1 = random.randrange(-10,11)
-	getal2 = random.randrange(-10,11)
-	antwoord = getal1 * getal2
-	print(getal1,'*',getal2,'=',antwoord)
-	i += 1
-	time.sleep(2)
-	os.system('cls')
+import os, random, time #Importeer de os, de random en de time module
+
+i = 0 #Zet de teller op 0
+while i < 5: #Ga door totdat de teller op vijf staat
+	getal1 = random.randrange(-10,11) #Genereer getal 1 met een range van -10 t/m 10
+	getal2 = random.randrange(-10,11) #Genereer getal 2 met een range van -10 t/m 10
+	
+	antwoord = getal1 * getal2 #Bepaal de uitkomst
+	print(getal1,'*',getal2,'=',antwoord) #Print de som met de uitkomst
+	i += 1 #Update de teller
+	
+	time.sleep(2) #Pauzeer voor 2 seconden
+	os.system('cls') #Leeg het scherm
+	continue #Terug naar het begin
